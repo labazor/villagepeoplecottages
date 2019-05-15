4a8183686a8fc57750a2531361e5a79601837ebd
Lassi Puurunen <puurunen.lassi@gmail.com>
2019-05-15 19:19:02 +0300

    Asiakkaan listaus, lisäys ja poisto

--- src/main/java/villagepeoplecottages/asiakas/AsiakasController.java
+++ src/main/java/villagepeoplecottages/asiakas/AsiakasController.java
@@ -1,9 +1,17 @@
 package villagepeoplecottages.asiakas;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import villagepeoplecottages.palvelu.Palvelu;
+import villagepeoplecottages.toimipiste.Toimipiste;
 
 @Controller
 public class AsiakasController {
@@ -19,5 +27,29 @@ public class AsiakasController {
 		model.addAttribute("asiakkaat", asiakasRepository.findAll());
 		
 		return "asiakas";
+	}
+	
+	@GetMapping("/asiakkaat/uusi")
+	public String uusiPalvelu(Model model) {
+		
+		return "asiakas_uusi";
+	}
+	
+	@PostMapping("asiakkaat/uusi")
+	public String postPalvelu(@RequestParam String enimi, @RequestParam String snimi,
+			@RequestParam String lahiosoite, @RequestParam String postinro, @RequestParam String postitoimipaikka,
+			@RequestParam String puhelinnro, @RequestParam String email) {
+		
+		Asiakas a = new Asiakas(enimi, snimi, lahiosoite, postitoimipaikka, postinro, email, puhelinnro, new ArrayList<>(), new ArrayList<>());
+		
+		asiakasRepository.save(a);
+		
+		return "redirect:/asiakkaat";
+	}
+	
+	@GetMapping("/asiakkaat/{id}/poista")
+	public String poistaPalvelu(@PathVariable Long id) {
+		asiakasRepository.deleteById(id);
+		return "redirect:/asiakkaat";
 	}
 }
